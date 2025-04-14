bf4dcf1bc58574f156497dc0ec206ca0b6b90653
KaykeTorres-Dev <kayketorres72@gmail.com>
2025-04-14 17:50:50 -0300

    Remoção de botões desncessários, limpeza de código, além de ajustes de layout em home

--- src/app/components/private/home/home.component.ts
+++ src/app/components/private/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnChanges {
+  constructor(){}
 
+  ngOnChanges(): void {
+    // debugger
+    // let menuIcon = document.getElementById('menu-icon') as HTMLElement;
+    // let navbar = document.getElementById('.navbar') as HTMLElement;
+
+    // menuIcon.onclick = () => {
+    //     menuIcon.classList.toggle('bx-x');
+    //     navbar.classList.toggle('active');
+    // };
+
+
+  }
 }
